c7ae01c769cc767aedc87417799802331180b276
Lars Avery <leaits@rit.edu>
2018-02-14 15:17:14 -0500

    updated for remove duplicates

--- bash/renameFilesWithMD5sum.sh
+++ bash/renameFilesWithMD5sum.sh
@@ -11,7 +11,7 @@ pathWithFiles=/tmp/testfiles
 
 #iterate over the files in the path(just the supplied path)
 for currentFilename in $pathWithFiles/*; do
-	echo $currentFilename
+	#echo $currentFilename
 
 	#get the path
 	#get the name
@@ -20,19 +20,29 @@ for currentFilename in $pathWithFiles/*; do
 	filename=$(basename "$currentFilename")
 	extension="${filename##*.}"
 	filename="${filename%.*}"
+
+	# for linux
 	md5=`md5sum ${currentFilename} | cut -d ' ' -f 1`
+	# for a mac
+	# md5=`md5 ${currentFilename} | cut -d ' ' -f 4`
 	#echo "Filename ${filename}  extension ${extension} md5sum ${md5}"
 
 	#create new filename
 	newFileName=${pathWithFiles}/${md5}.${extension}
-	echo "Current: ${currentFilename} New: ${newFileName}"
+	#echo "Current: ${currentFilename} New: ${newFileName}"
 
 	#verify new file doesn't already exist
 	if [ -a "$newFileName" ]
 	then
-		echo "file already exists"
+		# if current file != new file then delete
+		if [ "$currentFilename" != "$newFileName" ]
+		then
+			echo "$currentFilename is a duplicate, delete it"
+			rm $currentFilename
+		fi
 	else
 		# -n no clobber, -i interactive on overwrite (just being cautious)
+		echo "Moving: ${currentFilename} ${newFileName}"
 		mv -n -i $currentFilename $newFileName
 	fi
 
